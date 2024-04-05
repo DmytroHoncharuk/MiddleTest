6d6e106391baf0f649d74dda00e9075b67b28443
DmytroHoncharuk <dima2005oleksandr@gmail.com>
2024-04-05 11:41:30 +0300

    Added 2 tests for filter testing

---
+++ tests/test_filter_lines.py
@@ -0,0 +1,18 @@
+import pytest
+from main import filter_lines
+
+
+
+def test_filter_lines_returns_empty_list_when_keyword_is_not_present(prepare_input_file):
+    keyword = 'test'
+    result = filter_lines(prepare_input_file, keyword)
+    assert result == []
+
+
+@pytest.mark.parametrize("keyword, expected", [
+    ('hello', ['hello\n', 'hello world\n', 'helloworld']),
+    ('world', ['world\n', 'hello world\n', 'helloworld']),
+])
+def test_filter_lines_returns_filtered_lines(prepare_input_file, keyword, expected):
+    result = filter_lines(prepare_input_file, keyword)
+    assert result == expected

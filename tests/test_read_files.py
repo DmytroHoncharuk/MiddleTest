823670c54952f8ed138fdfb61d03fbedd91dd0a3
DmytroHoncharuk <dima2005oleksandr@gmail.com>
2024-04-05 11:01:01 +0300

    Create test_read_files_non_existing_file test

--- tests/test_read_files.py
+++ tests/test_read_files.py
@@ -1,9 +1,10 @@
 import pytest
 from main import read_files
 import os
+
+
 @pytest.fixture
 def create_test_file(tmpdir):
-    # Create a temporary test file
     test_file_path = os.path.join(tmpdir, 'test_file.txt')
     with open(test_file_path, 'w', encoding='utf-8') as f:
         f.write("Hello world!\n")
@@ -13,7 +14,13 @@ def create_test_file(tmpdir):
         f.write("Goodbye world!\n")
     return test_file_path
 
+
 def test_read_files_existing_file(create_test_file):
     file_path = create_test_file
     expected_contents = "Hello world!\nThis is a test file containing the word \"world\".\nWe want to filter out the lines containing the word \"world\".\nLet's see if our script can do that.\nGoodbye world!\n"
     assert read_files(file_path) == expected_contents
+
+
+def test_read_files_non_existing_file(tmpdir):
+    file_path = os.path.join(tmpdir, 'non_existing_file.txt')
+    assert read_files(file_path) is None
